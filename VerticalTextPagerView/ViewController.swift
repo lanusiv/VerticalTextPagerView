664a63723176dbe71754121e7a7938097d4d1931
leray <visunal@163.com>
2018-05-08 10:48:03 +0800

    Initial Commit

---
+++ VerticalTextPagerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VerticalTextPagerView
+//
+//  Created by Leray Lanusiv on 5/8/18.
+//  Copyright © 2018 Leray Lanusiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
